58d0c6e66d3b843f1efe204d0f5b4682fbcc10eb
Samir_79 <samirovsky79@gmail.com>
2021-11-29 13:54:15 +0100

    Initializing projektarbete

---
+++ src/main/java/se/iths/projektarbetekomplexjava/ProjektArbeteKomplexJavaApplication.java
@@ -0,0 +1,13 @@
+package se.iths.projektarbetekomplexjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektArbeteKomplexJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektArbeteKomplexJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/projektarbetekomplexjava/ProjektArbeteKomplexJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.projektarbetekomplexjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektArbeteKomplexJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
